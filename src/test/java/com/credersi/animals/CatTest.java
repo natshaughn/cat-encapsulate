f0a68da8f8986601b62ea626a3e166b5ff44407e
nathan.shaughnessy <nathan.shaughnessy@ROQ-319.roq.local>
2023-02-22 10:56:25 +0000

    Changes to tests with the new class

--- src/test/java/com/credersi/animals/CatTest.java
+++ src/test/java/com/credersi/animals/CatTest.java
@@ -12,4 +12,77 @@ public class CatTest {
 		assertTrue(cat.isAlive());
 		assertEquals(cat.howHungry(), 50);
 	}
+	@Test 
+	public void testJelly() {
+		assert(true);
+	}
+	@Test
+	public void testFeeding() {
+		Cat cat = new Cat();
+		cat.feeds();
+		assertEquals(cat.howHungry(), 30);
+		
+	}
+	@Test 
+	public void testRunning() {
+		Cat cat = new Cat();
+		cat.runs();
+		assertEquals(cat.howHungry(), 80);
+	}
+	@Test 
+	public void testOverFeeding() {
+		Cat cat = new Cat();
+		cat.feeds();
+		cat.feeds();
+		cat.feeds();
+		assertEquals(cat.howHungry(), 0);
+	}
+	@Test
+	public void testFeedAndRun() {
+		Cat cat = new Cat();
+		cat.feeds();
+		cat.runs();
+		assertEquals(cat.howHungry(), 60);	
+	}
+	@Test 
+	public void testRunTwice() {
+		Cat cat = new Cat();
+		cat.runs();
+		cat.runs();
+		assertEquals(cat.howHungry(), 50);
+		assertTrue(cat.isAlive() == true);
+	}
+	@Test 
+	public void testRunThreeTimes() {
+		Cat cat = new Cat();
+		for (int i = 0; i < 3; i++) {
+			cat.runs();
+		}
+		assertEquals(cat.howHungry(), 80);
+		assertTrue(cat.isAlive() == true);
+	}
+	@Test 
+	public void testEachFunctionTenTImes() {
+		Cat cat = new Cat();
+		for (int i = 0; i < 10; i++) {
+			cat.runs();
+			cat.feeds();
+		}
+		assertTrue(cat.isAlive());
+	}
+	@Test
+	public void testRunFuncTwentytimes() {
+		Cat cat = new Cat();
+		for (int i = 0; i < 20; i++) {
+			cat.runs();
+		}
+		assertTrue(!cat.isAlive());
+	}
+	@Test public void testRunFuncTenTimes() {
+		Cat cat = new Cat();
+		for (int i = 0; i < 10; i++) {
+			cat.runs();
+		}
+		assertTrue(cat.isAlive());
+	}
 }
